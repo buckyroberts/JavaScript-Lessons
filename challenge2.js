47731fb03fce95cf4a2d75af2dbd2e0ef2abaff1
Bucky <buckyroberts@gmail.com>
2016-07-01 01:49:44 -0400

    Updated code

--- challenge2.js
+++ challenge2.js
@@ -2,11 +2,10 @@ function caseChanger(str) {
     var arr = str.split('');
     var result = '';
     for (var i = 0; i < arr.length; i++) {
-        if(i % 2 != 0) {
+        if (i % 2 != 0)
             result += arr[i].toUpperCase();
-        }else{
+        else
             result += arr[i].toLowerCase();
-        }
     }
     return result;
 }
