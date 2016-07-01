5594031d650dbc9addde8622c8d11f8f6aa6bbcf
Bucky <buckyroberts@gmail.com>
2016-07-01 01:45:20 -0400

    Updated code

---
+++ challenge2.js
@@ -0,0 +1,14 @@
+function caseChanger(str) {
+    var arr = str.split('');
+    var result = '';
+    for (var i = 0; i < arr.length; i++) {
+        if(i % 2 != 0) {
+            result += arr[i].toUpperCase();
+        }else{
+            result += arr[i].toLowerCase();
+        }
+    }
+    return result;
+}
+
+console.log(caseChanger('buckyroberts'));
