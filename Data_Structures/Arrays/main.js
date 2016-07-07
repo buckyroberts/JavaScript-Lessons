7d5b6a3e905827b33a5cef40db64515d05430f22
buckyroberts <buckyroberts@gmail.com>
2016-07-07 05:15:06 -0500

    Working on courses

--- Data_Structures/Arrays/main.js
+++ Data_Structures/Arrays/main.js
@@ -1,4 +1,4 @@
-var food = ['bacon', 'corn', 'sausage'];
+var food = ['bacon', 'corn', 'ham', 'sausage'];
 
 // array values are retrieved by index, not name
 console.log(food[2]);
@@ -17,22 +17,24 @@ food.forEach(function (index, item) {
 
 console.log('----------');
 
-// add new items to an array
+// add new items to end of array
 food.push('tuna');
 console.log(food);
 
-// remove item from the end of an array
+// remove item from the end of array
 var lastItem = food.pop();
 console.log(lastItem);
+console.log(food);
+
+console.log('----------');
+
+// add item to beginning of an array
+food.unshift('apples');
 console.log(food);
 
 // remove item from the beginning of an array
 var firstItem = food.shift();
 console.log(firstItem);
-console.log(food);
-
-// add item to beginning of an array
-food.unshift('apples');
 console.log(food);
 
 // remove item by index position
