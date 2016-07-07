8cebee44cf046b95d3c5e6ce86bc9f76bd4f8699
Bucky <buckyroberts@gmail.com>
2016-07-07 02:23:52 -0400

    Updated README

--- ES6/Spread_Operator/main.js
+++ ES6/Spread_Operator/main.js
@@ -4,5 +4,11 @@ function addNumbers(a, b, c){
     console.log(a + b + c);
 }
 
-var nums = [1, 2, 3];
+// expand array into function arguments
+var nums = [3, 4, 5];
 addNumbers(...nums);
+
+// used to combine arrays
+var meats = ['bacon', 'ham'];
+var food = ['apples', ...meats, 'kiwi', 'rice'];
+console.log(food);
